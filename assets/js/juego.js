afdd2687a132bbc2e840ccb6702058f87dacc875
EngelberAmaya <engelberamaya@gmail.com>
2023-04-07 13:32:07 -0400

    Value of each letter

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -49,4 +49,27 @@ const pedirCarta = () => {
     return carta;
 }
 
-pedirCarta();
+//pedirCarta();
+
+const valorCarta = (carta) => {
+
+    const valor = carta.substring(0, carta.length - 1);
+    
+    return (isNaN(valor)) ?
+            (valor === 'A') ? 11 : 10
+            : valor * 1;
+
+
+    /*if (isNaN(valor)) {
+       
+        puntos = (valor === 'A') ? 11 : 10;
+
+    } else {
+        puntos = valor * 1;
+    }*/
+
+
+}
+
+const valor = valorCarta(pedirCarta());
+console.log({valor});
