1c1da610844d3e4ce0cec9f0c4b0261f2bb9acc9
EngelberAmaya <engelberamaya@gmail.com>
2023-04-07 17:57:54 -0400

    Event click - make a letter

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -7,7 +7,15 @@
 
 let deck = [];
 const tipos = ['C', 'D', 'H', 'S'];
-const especiales = ['A', 'J', 'Q', 'K']
+const especiales = ['A', 'J', 'Q', 'K'];
+
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+// Referencias del HTML
+const btnPedir = document.querySelector('#btnPedir');
+
+const puntosHTML =  document.querySelectorAll('small');
 
 const crearDeck = () => {
 
@@ -26,10 +34,7 @@ const crearDeck = () => {
         }
     }
 
-    //console.log(deck);
     deck = _.shuffle(deck);
-    console.log(deck);
-
     return deck;
 }
 
@@ -42,9 +47,6 @@ const pedirCarta = () => {
     }
 
     const carta = deck.pop();
-
-    console.log(deck);
-    console.log(carta);
 
     return carta;
 }
@@ -71,5 +73,17 @@ const valorCarta = (carta) => {
 
 }
 
-const valor = valorCarta(pedirCarta());
-console.log({valor});
+//const valor = valorCarta(pedirCarta());
+//console.log({valor});
+
+// Eventos
+
+btnPedir.addEventListener('click', () => {
+
+    const carta = pedirCarta();
+
+    puntosJugador = puntosJugador + valorCarta(carta);
+
+    puntosHTML[0].innerText = puntosJugador;
+   
+});
